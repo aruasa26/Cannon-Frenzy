63a1827895949025db35189300c389a0000673c1
Fidel Isaboke <fidelisaboke@gmail.com>
2024-11-24 20:01:09 +0300

    Add level configuration

---
+++ levels_config.py
@@ -0,0 +1,28 @@
+""" This module contains level definitions for the game.
+Level parameters: level_number, num_targets, target_size, cannonball_power, cannonballs_left
+ """
+
+LEVELS_CONFIG = [
+    {
+        "level_number": 1,
+        "num_targets": 5,
+        "target_size": 50,
+        "cannonball_power": 30,
+        "cannonballs_left": 7
+    },
+    {
+        "level_number": 2,
+        "num_targets": 7,
+        "target_size": 45,
+        "cannonball_power": 35,
+        "cannonballs_left": 8
+    },
+    {
+        "level_number": 3,
+        "num_targets": 10,
+        "target_size": 40,
+        "cannonball_power": 40,
+        "cannonballs_left": 9
+    }
+]
+
